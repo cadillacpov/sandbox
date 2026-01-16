3ec4cf96bc056bb498a2cd4d941300140ffa9d0b
cadillacpov <218366720+cadillacpov@users.noreply.github.com>
2026-01-16 12:49:03 +0530

    hotfix: rm public modifiers

--- Learn Java/src/day1/Multiplier.java
+++ Learn Java/src/day1/Multiplier.java
@@ -3,7 +3,7 @@ package day1;
 import java.util.Scanner;
 
 public class Multiplier {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter first integer: ");
